59deaec49bdd45b4af089de702481b9aa1e0727f
Chris Morin <longhumansbiz@gmail.com>
2022-06-14 11:59:15 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let arrowClick = document.querySelector(".fa-circle-arrow-down")
+let box = document.querySelector(".imgtest")
+arrowClick.addEventListener("click", expand)
+
+
+function expand(){
+    document.querySelector(".imgtest").style.height = "200px";
+    box.addEventListener("mouseout", close)
+    function close(){
+        setTimeout(function() {
+            box.style.height = "50px";
+          }, 500);
+    }
+    }
+
+
+
+
+
+
+
