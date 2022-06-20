4006bc2ae401917e4643f9fac22e7c090cc27e8e
Chris Morin <longhumansbiz@gmail.com>
2022-06-20 09:14:35 -0600

    Added CSS button, worked on dropdown

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-let arrowClick = document.querySelector(".fa-circle-arrow-down")
+let arrowClick = document.querySelector(".arrows")
 let box = document.querySelector(".imgtest")
 arrowClick.addEventListener("click", expand)
 
 
 function expand(){
-    document.querySelector(".imgtest").style.height = "300px";
+    document.querySelector(".imgtest").style.height = "500px";
     box.addEventListener("mouseout", close)
     function close(){
         setTimeout(function() {
-            box.style.height = "50px";
+            box.style.height = "100px";
           }, 500);
     }
     }
+
 
 
 
